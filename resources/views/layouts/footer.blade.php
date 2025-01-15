8a6d031fe78da56a9a115d0d4cda2a7cd88aa65e
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-01-15 17:01:24 +0100

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,41 +1,127 @@
 <footer>
-    <div class="footer-container">
-        <div class="flex flex-col items-center gap-y-8">
-            <div class="logo footer-logo">
-                <x-logo-footer />
-                <x-social-icons />
+    <div class="footer max-w-screen-xl mx-auto px-4">
+        <div class="identity">
+            <div class="footer-logo">
+                <x-application-logo />
             </div>
-            <ul class="footer-menu">
-                
+            <x-social-icons />
+        </div>
+        <div class="flex flex-col md:flex-row justify-between gap-8">
+            <div class="footer-menu">
+                <ul>
+                    <li x-data="{ open: false, isMd: window.matchMedia('(min-width: 768px)').matches }"
+                        x-init="
+                            const updateIsMd = () => isMd = window.matchMedia('(min-width: 768px)').matches;
+                            window.addEventListener('resize', updateIsMd);
+                            updateIsMd();
+                        "
+                        class="relative">
+                        <a
+                            @click="if (!isMd) open = !open"
+                            :class="{'w-full': open}"
+                            class="flex justify-between cursor-pointer"
+                        >
+                            <p>Legales</p><x-icons.chevron-down />
+                        </a>
+                        <ul @click.outside="if (!isMd) open = false"
+                            x-show="open || isMd"
+                            x-transition
+                            x-cloak
+                            class="footer-sub-menu dropdown md:block"
+                        >
+                            <li>
+                                <a href=""><p>Términos y condiciones</p></a>
+                            </li>
+                            <li>
+                                <a href=""><p>Política de cookies</p></a>
+                            </li>                        
+                        </ul>
+                    </li>
+
+                    <li x-data="{ open: false, isMd: window.matchMedia('(min-width: 768px)').matches }"
+                        x-init="
+                            const updateIsMd = () => isMd = window.matchMedia('(min-width: 768px)').matches;
+                            window.addEventListener('resize', updateIsMd);
+                            updateIsMd();
+                        "
+                        class="relative">
+                        <a
+                            @click="if (!isMd) open = !open"
+                            :class="{'w-full': open}"
+                            class="flex justify-between cursor-pointer"
+                        >
+                            <p>Publicá con nosotros</p><x-icons.chevron-down />
+                        </a>
+                        <ul @click.outside="if (!isMd) open = false"
+                            x-show="open || isMd"
+                            x-transition
+                            x-cloak
+                            class="footer-sub-menu dropdown md:block"
+                        >
+                            <li>
+                                <a href=""><p>Beneficios de publicar en El Vocerito</p></a>
+                            </li>
+                            <li>
+                                <a href=""><p>Preguntas Frecuentes</p></a>
+                            </li>                        
+                        </ul>
+                    </li>
+
+                    <li x-data="{ open: false, isMd: window.matchMedia('(min-width: 768px)').matches }"
+                        x-init="
+                            const updateIsMd = () => isMd = window.matchMedia('(min-width: 768px)').matches;
+                            window.addEventListener('resize', updateIsMd);
+                            updateIsMd();
+                        "
+                        class="relative">
+                        <a
+                            @click="if (!isMd) open = !open"
+                            :class="{'w-full': open}"
+                            class="flex justify-between cursor-pointer"
+                        >
+                            <p>Sobre El Vocerito</p><x-icons.chevron-down />
+                        </a>
+                        <ul @click.outside="if (!isMd) open = false"
+                            x-show="open || isMd"
+                            x-transition
+                            x-cloak
+                            class="footer-sub-menu dropdown md:block"
+                        >
+                            <li>
+                                <a href=""><p>Un poco de historia</p></a>
+                            </li>
+                            <li>
+                                <a href=""><p>Cómo usar la guía</p></a>
+                            </li>                        
+                        </ul>
+                    </li>
+                </ul>            
+            </div>
+            <div class="footer-subscription">
+                <p>Suscribite a nuestro newsletter</p>
+                <form action="#" class="form">
+                    @csrf
+                    <div class="relative w-full">
+                        <input id="nameInput" type="text" name="name" placeholder="Email" required class="pl-10 w-full">
+                        <div class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+                            <x-icons.mail />
+                        </div>
+                    </div>
+                    <x-button class="btn btn-primary">Suscribirme</x-button>
+                </form>
+            </div>
+        </div>
+        <div class="need_help">
+            <ul>
+                <li><p class="text-gray_500 ">¿Necesitás ayuda?</p></li>
+                <li><a href=""><p>Contactanos</p></a></li>
             </ul>
-            <div class="flex gap-4">
-                <p><a href="/politica-de-privacidad">{{ __('Política de Privacidad') }}</a></p>
-                <p><a href="/terminos-y-condiciones">{{ __('Términos y Condiciones') }}</a></p>
-            </div>
-            <p class="">Fuengirola, Málaga.</p>
         </div>
     </div>
-    <hr class="footer-divider" />
-    <div class="post-footer">
-        <span class="developed-by">{{__('Sitio diseñado y desarrollado por')}}<a href="https://nicolasgelabert.com.ar" target="_blank"> Nicolás Gelabert</a></span>
-        <ul class="flex gap-x-4">
-            @foreach (Config::get('languages') as $lang => $language)
-                @if ($lang != App::getLocale())
-                    <li>
-                        <a class="flex items-center gap-x-1 opacity-50" href="{{ route('lang.switch', $lang) }}">
-                            <span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
-                            <span>{{$language['display']}}</span>
-                        </a>
-                    </li>
-                @else
-                    <li>
-                        <a class="flex items-center gap-x-1" href="{{ route('lang.switch', $lang) }}">
-                            <span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
-                            <span>{{$language['display']}}</span>
-                        </a>
-                    </li>
-                @endif
-            @endforeach
-        </ul>
+    <div class="max-w-screen-xl mx-auto px-4">
+        <div class="post_footer">
+            <p class="text-small">©Todos los derechos reservados.</p>
+            <p class="text-small">Diseño y desarrollo por <a href="">Chimi Creativo</a></p>
+        </div>
     </div>
 </footer>
