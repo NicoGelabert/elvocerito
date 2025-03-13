41c387c06b6a8a29d3f519ef36a3b5bcfd647032
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-03-13 11:13:36 +0100

    href en contact-icons

--- resources/views/components/contact-icons.blade.php
+++ resources/views/components/contact-icons.blade.php
@@ -1,13 +1,28 @@
 @props(['icons' => []])
 <div {{ $attributes->merge(['class' => '']) }}>
     @foreach ($icons as $iconData)
-        <div class="flex items-center md:gap-4">
-            <x-icons.icon :name="$iconData['type']" />
-            <div>
-                <span class="text-sm font-semibold capitalize hidden md:block">{{ $iconData['type'] }}</span>
-                <p class="text-xs text-gray-500 hidden md:block">{{ $iconData['info'] }}</p>
+        @php
+            $href = '#'; // Valor por defecto si no coincide con ninguna condición
+            
+            if ($iconData['type'] === 'fijo' || $iconData['type'] === 'movil') {
+                $href = "tel:{$iconData['info']}";
+            } elseif ($iconData['type'] === 'whatsapp') {
+                $href = "https://wa.me/" . preg_replace('/\D/', '', $iconData['info']); // Solo números
+            } elseif ($iconData['type'] === 'email') {
+                $href = "mailto:{$iconData['info']}";
+            } elseif ($iconData['type'] === 'facebook' || $iconData['type'] === 'instagram') {
+                $href = $iconData['info']; // URL directa de la red social
+            }
+        @endphp
+        <a href="{{ $href }}">
+            <div class="flex items-center md:gap-4">
+                <x-icons.icon :name="$iconData['type']" />
+                <div>
+                    <span class="text-sm font-semibold capitalize hidden md:block">{{ $iconData['type'] }}</span>
+                    <p class="text-xs text-gray-500 hidden md:block">{{ $iconData['info'] }}</p>
+                </div>
             </div>
-        </div>
+        </a>
     @endforeach
 </div>
 
