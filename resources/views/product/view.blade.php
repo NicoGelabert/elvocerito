f9675166f54b61cf2ddcc91968d6c521a729464a
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-03-24 10:35:55 +0100

    Vista anunciantes parte III

--- resources/views/product/view.blade.php
+++ resources/views/product/view.blade.php
@@ -1,5 +1,12 @@
 @php
     use Carbon\Carbon;
+    $hoy = strtolower(Carbon::now()->locale('es')->isoFormat('dddd')); // Día de hoy en minúsculas
+    $horarios = $product->horarios->sortBy(fn($h) => array_search(strtolower($h->dia), [
+        "lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"
+    ]));
+
+    // Buscar el horario de hoy o el siguiente disponible
+    $horarioHoy = $horarios->firstWhere('dia', ucfirst($hoy)) ?? $horarios->first();
 @endphp
 <x-app-layout>
     <div id="product-view"
@@ -12,6 +19,7 @@
         'addresses' => $product->addresses,
         'images' => $product->images->pluck('url') 
         ]) }})">
+        <!-- INICIO MENU CON IMAGEN, TITLE, BAGDE ABI/CER, CONTACT -->
         <div class="product_menu opacity-0 -translate-y-full">
             <div class="container flex justify-between">
                 <div class="flex gap-4">
@@ -44,8 +52,11 @@
                 <x-contact-icons class="contact-icons" :icons="$product->contacts"></x-contact-icons>
             </div>
         </div>
+        <!-- FIN MENU CON IMAGEN, TITLE, BAGDE ABI/CER, CONTACT -->
+        <!-- INICIO HOJA PRODUCTO -->
         <div class="product">
-            <div class="product_view_header">
+            <!-- INICIO BREADCRUMBS -->
+            <div class="breadcrumbs">
                 <div class="container flex gap-2">
                     <a href="/">
                         <x-icons.home class="fill-gray_400" />
@@ -63,53 +74,26 @@
                     <h2 class="text-small">{{ $subcategory->name }}</h2>
                 </div>
             </div>
-
+            <!-- FIN BREADCRUMBS -->
+            <!-- INICIO PRIMERA FILA -->
             <div class="flex gap-8 container">
+                <!-- INICIO ESPACIO VACIO -->
                 <div class="hidden lg:block w-1/4"></div>
+                <!-- FIN ESPACIO VACIO -->
+                <!-- INICIO TITLE -->
                 <div class="mb-10 w-full lg:w-3/4 text-center lg:text-left">
                     <h2>{{ $product->title }}</h2>
                 </div>
+                <!-- FIN TITLE -->
             </div>
+            <!-- FIN PRIMERA FILA -->
+            <!-- INICIO SEGUNDA FILA -->
             <div class="flex flex-col lg:flex-row gap-8 container">
+                <!-- INICIO SF PRIMERA COLUMNA -->
                 <div class="block overflow-x-auto scroll-smooth w-full lg:w-1/4">
-                    <div class="flex lg:flex-col gap-6 horizontal_scrolling_container">
-                        <div class="product_tags">
-                            @if ($product->tags->isNotEmpty())
-                            <h5>Etiquetas</h5>
-                            <div class="tags">
-                                @foreach($product->tags as $tag)
-                                <x-badge badge_title="{{ $tag->name }}" />
-                                @endforeach
-                            </div>
-                            @endif
-                        </div>
-                        @if ($product->horarios->isNotEmpty())
-                        <div class="product_opening_hours md:col-span-1">
-                            <h5>Horario de atención</h5>
-                            <div class="product_opening_hours_content">
-                                <div class="flex flex-col gap-3">
-                                
-                                @foreach ($product->horarios as $horario)
-                                    @php
-                                        $apertura = Carbon::parse($horario->apertura)->format('H:i');
-                                        $cierre = Carbon::parse($horario->cierre)->format('H:i');
-                                    @endphp
-                                    <div class="flex gap-2 items-center"
-                                        x-data="verificarHorario('{{ strtolower($horario->dia) }}', '{{ $apertura }}', '{{ $cierre }}')">
-                                        <p>{{ ucfirst($horario->dia) }} {{ $apertura }} a {{ $cierre }}</p>
-                                        <template x-if="esHoy">
-                                            <x-badge x-bind:class="(estado === 'Cerrado' ? 'closed' : (estado === 'Abierto' ? 'open' : ''))">
-                                                <span x-text="estado"></span> 
-                                            </x-badge>
-                                        </template>
-                                    </div>
-                                @endforeach
-    
-                                </div>
-                                <x-icons.chevron-down />
-                            </div>
-                        </div>
-                        @endif
+                    <!-- INICIO CONTENEDOR HORIZONTAL: MOBILE -->
+                    <div class="horizontal_scrolling_container">
+                        <!-- INICIO RESEÑAS -->
                         <div class="product_rating md:col-span-1">
                             <div class="hidden lg:block">
                                 <h5>Reseñas</h5>
@@ -122,12 +106,80 @@
                                 <x-icons.chevron-down />
                             </div>
                         </div>
+                        <!-- FIN RESEÑAS -->
+                        <!-- INICIO HORARIOS -->
+                        @if ($product->horarios->isNotEmpty())
+                        <div class="product_opening_hours md:col-span-1">
+                            <h5>Horario de atención</h5>
+                            <div class="product_opening_hours_content" x-data="{ modalAbierto: false }">
+                                <div class="flex flex-col gap-3">
+                                    @if ($horarioHoy)
+                                        @php
+                                            $apertura = Carbon::parse($horarioHoy->apertura)->format('H:i');
+                                            $cierre = Carbon::parse($horarioHoy->cierre)->format('H:i');
+                                        @endphp
+                                        <div class="flex gap-2 items-center lg:px-4"
+                                            x-data="verificarHorario('{{ strtolower($horarioHoy->dia) }}', '{{ $apertura }}', '{{ $cierre }}')">
+                                            <p>{{ ucfirst($horarioHoy->dia) }} {{ $apertura }} - {{ $cierre }}</p>
+                                            <template x-if="esHoy">
+                                                <x-badge x-bind:class="(estado === 'Cerrado' ? 'closed' : 'open')">
+                                                    <span x-text="estado"></span>
+                                                </x-badge>
+                                            </template>
+                                        </div>
+                                    @else
+                                        <p>No hay horarios disponibles.</p>
+                                    @endif
+                                </div>
+
+                                <!-- Botón para abrir modal -->
+                                <button @click="modalAbierto = true" class="px-3">
+                                    <x-icons.chevron-down />
+                                </button>
+
+                                <!-- Modal con todos los horarios -->
+                                <div x-show="modalAbierto" class="horario_modal">
+                                    <div 
+                                        class="horario_modal_content">
+                                        <h4 class="lg:hidden">Horario de atención</h4>
+                                        <div class="flex items-start justify-between gap-4 relative">
+                                            <ul>
+                                                @foreach ($product->horarios as $horario)
+                                                    <li><p>{{ ucfirst($horario->dia) }} 
+                                                        {{ Carbon::parse($horario->apertura)->format('H:i') }} - 
+                                                        {{ Carbon::parse($horario->cierre)->format('H:i') }}</p>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                            <button @click="modalAbierto = false">✕</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                        <!-- FIN HORARIOS -->
+                        <!-- INICIO TAGS -->
+                        @if ($product->tags->isNotEmpty())
+                        <div class="product_tags">
+                            <h5>Etiquetas</h5>
+                            <div class="tags">
+                                @foreach($product->tags as $tag)
+                                <x-badge badge_title="{{ $tag->name }}" />
+                                @endforeach
+                            </div>
+                        </div>
+                        @endif
+                        <!-- FIN TAGS -->
+                        <!-- INICIO ADDRESSES -->
                         @if ($product->addresses->isNotEmpty())
                         <div class="product_location">
                             <h5>Ubicación</h5>
                             <x-addresses :addresses="$product->addresses"></x-addresses>
                         </div>
                         @endif
+                        <!-- FIN ADDRESSES -->
+                        <!-- INICIO SOCIALS -->
                         @if ($product->socials->isNotEmpty())
                         <div class="product_rrss md:col-span-1">
                             <h5>Redes</h5>
@@ -136,9 +188,12 @@
                             </div>
                         </div>
                         @endif
+                        <!-- FIN SOCIALS -->
                     </div>
+                    <!-- INICIO CONTENEDOR HORIZONTAL: MOBILE -->
                 </div>
-                <!-- Inicio Columna derecha -->
+                <!-- FIN SF PRIMERA COLUMNA -->
+                <!-- INICIO SF SEGUNDA COLUMNA -->
                 <div class="w-full lg:w-3/4 flex flex-col gap-10">
                     <div class="flex flex-col lg:flex-row gap-4">
                         <div class="product_header flex-1">
@@ -205,9 +260,11 @@
                         </div>
                     </div>
                 </div>
-                <!-- Fin Columna derecha -->
+                <!-- FIN SF PRIMERA COLUMNA -->
             </div>
+            <!-- INICIO SEGUNDA FILA -->
         </div>
+        <!-- FIN HOJA PRODUCTO -->
     </div>
 </x-app-layout>
 
