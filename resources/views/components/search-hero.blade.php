169dcf6438da07f25365eecd81e712bafc8a0852
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-04-07 09:56:40 +0200

    Search hero banner fix II, pinmaps

--- resources/views/components/search-hero.blade.php
+++ resources/views/components/search-hero.blade.php
@@ -1,10 +1,10 @@
 <div class="search_hero">
-    <div class="max-w-screen-xl w-full flex justify-center lg:justify-end mx-auto">
+    <x-backgrounds.pinmaps />
+    <div class="absolute max-w-screen-xl w-full flex justify-center lg:justify-end mx-auto">
         <div class="search_hero_container">
             <h1>Encontrá lo que necesitás en Zona Sur</h1>
             <p>Más de 200 empresas y prestadores de servicio cerca!</p>
             <x-search></x-search>
         </div>
     </div>
-    <x-backgrounds.pinmaps />
 </div>
