a372ffb04c025b80727ce92afa31c120caef8f79
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-04-25 15:19:31 +0200

    Design fixes home IV

--- resources/views/components/news.blade.php
+++ resources/views/components/news.blade.php
@@ -13,39 +13,35 @@
         <div class="md:w-1/2">
 
         </div>
-        <div class="news splide">
-            <div class="splide__track">
-                <ul class="splide__list">
-                    @foreach($articles as $article)
-                    <li class="news-card splide__slide">
-                        <a href="{{ route('news.view', $article) }}">
-                            <div class="flex gap-4 p-2 md:p-6 bg-white rounded-xl">
-                                <div class="news-card-img">
-                                    <img src="{{ $article->image }}" alt="$article->title">
-                                </div>
-                                <div class="news-card-content">
-                                    <h5>{{ $article->title }}</h5>
-                                    <div class="news-card-content-header">
-                                        <div>
-                                            @foreach ($article->authors as $author)
-                                            <div class="author">
-                                                <img src="{{ $author->image }}" alt="$author->name">
-                                                <h6>{{ $author->name }}</h6>
-                                            </div>
-                                            @endforeach
-                                        </div>
-                                        <p class="dot-divider">·</p>
-                                        <p class="news-date">{{ $article->created_at->format('M d, Y') }}</p>
+        <div class="news">
+            <ul>
+                @foreach($articles as $article)
+                <a href="{{ route('news.view', $article) }}">
+                    <li class="news-card">
+                        <div class="news-card-img">
+                            <img src="{{ $article->image }}" alt="$article->title">
+                        </div>
+                        <div class="news-card-content">
+                            <h5>{{ $article->title }}</h5>
+                            <div class="news-card-content-header">
+                                <div>
+                                    @foreach ($article->authors as $author)
+                                    <div class="author">
+                                        <img src="{{ $author->image }}" alt="$author->name">
+                                        <h6>{{ $author->name }}</h6>
                                     </div>
-                                    <!-- <p class="description">{{ $article->news_lead }}</p> -->
-                                    <x-badge badge_title="#PalabrasDeExpertos" />
+                                    @endforeach
                                 </div>
+                                <p class="dot-divider">·</p>
+                                <p class="news-date">{{ $article->created_at->format('M d, Y') }}</p>
                             </div>
-                        </a>
+                            <!-- <p class="description">{{ $article->news_lead }}</p> -->
+                            <x-badge badge_title="#PalabrasDeExpertos" />
+                        </div>
                     </li>
-                    @endforeach
-                </ul>
-            </div>
+                </a>
+                @endforeach
+            </ul>
         </div>
     </div>
 </div>
