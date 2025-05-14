585aeac3af5dc14cd5af28edfd0176c4e92b8776
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-05-14 12:04:37 +0200

    Descarga de listado de productos en excel III

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://goquickly.online', 'http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => ['https://admin.goquickly.online', 'http://localhost:5173', 'http://localhost:5174'],
 
     'allowed_origins_patterns' => ['*'],
 
