0512f7dfa7a2078370e356e39d82912616ed46e5
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-05-16 10:17:07 +0200

    Cambio en el search hero de categorías

--- resources/views/components/search-hero.blade.php
+++ resources/views/components/search-hero.blade.php
@@ -1,6 +1,6 @@
 <div class="search_hero">
     <x-backgrounds.pinmaps />
-    <div class="max-w-screen-lg w-full flex justify-center lg:justify-end mx-auto mb-20">
+    <div class="max-w-screen-lg w-full flex justify-center lg:justify-end mx-auto">
         <div class="search_hero_container">
             <h1>Encontrá lo que necesitás en Zona Sur</h1>
             <p>Más de 200 empresas y prestadores de servicio cerca!</p>
