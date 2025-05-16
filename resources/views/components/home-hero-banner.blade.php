13c94d34f7d98f70c7e397f6ae37a3b27a33a6f4
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-05-16 15:38:45 +0200

    Rediseño Home Hero Banner fix

--- resources/views/components/home-hero-banner.blade.php
+++ resources/views/components/home-hero-banner.blade.php
@@ -12,9 +12,11 @@
                             <h5>{{ $homeherobanner->headline }}</h5>
                             <h3>{{ $homeherobanner->description }}</h3>
                             <p>{{ $homeherobanner->short_description }}</p>
-                            @foreach ($homeherobanner->tags as $tag)
-                            <x-badge badge_title="{{ $tag->name }}"/>
-                            @endforeach
+                            <div class="flex gap-4">
+                                @foreach ($homeherobanner->tags as $tag)
+                                <x-badge badge_title="{{ $tag->name }}"/>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </a>
