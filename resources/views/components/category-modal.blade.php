7b68445480725d78583003b5d231a15c7a6adbf1
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-06-17 15:56:37 +0200

    Modals fix II

--- resources/views/components/category-modal.blade.php
+++ resources/views/components/category-modal.blade.php
@@ -18,7 +18,7 @@
       x-transition:leave-start="opacity-100 translate-y-0"
       x-transition:leave-end="opacity-0 translate-y-full"
       @click.stop
-      class="bg-white p-6 rounded-t-lg shadow-xl max-w-screen-xl w-full h-full overflow-auto"
+      class="bg-white p-6 rounded-t-lg md:rounded-lg shadow-xl max-w-screen-xl w-full h-full overflow-auto"
     >
       <div class="flex items-center justify-between mb-4">
         <h2 class="text-xl font-bold">Categorías</h2>
