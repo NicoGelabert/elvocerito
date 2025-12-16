2fb336c300a19a3e790b69ee3648d4f03c7fac71
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-12-16 13:15:41 -0300

    Small fixes

--- resources/views/components/anunciantes-destacados.blade.php
+++ resources/views/components/anunciantes-destacados.blade.php
@@ -50,7 +50,7 @@
                         </a>
                         @if ($anunciante_destacado->reviews_count > 0)
                         <!-- Promedio de calificaciones -->
-                        <div class="flex items-center gap-2 justify-center">
+                        <div class="flex flex-col md:flex-row items-center gap-2 justify-center">
                             <div
                                 class="product-rating-average product_rating"
                                 data-product-id="{{ $anunciante_destacado->id }}">
