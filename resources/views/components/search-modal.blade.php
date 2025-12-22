23a9c9026dccda857063ee127a328292b57c78bc
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-12-22 14:50:18 -0300

    Search fix II

--- resources/views/components/search-modal.blade.php
+++ resources/views/components/search-modal.blade.php
@@ -3,11 +3,9 @@
   x-init="
     window.addEventListener('open-search-modal', () => {
       isOpen = true
-      $nextTick(() => {
-        setTimeout(() => {
-          $refs.searchInput?.focus()
-        }, 50)
-      })
+      if (window.innerWidth >= 768) {
+        $nextTick(() => $refs.searchInput.focus())
+      }
     })
   "
 >
@@ -27,6 +25,7 @@
       x-transition:leave="transition transform duration-300"
       x-transition:leave-start="opacity-100 translate-y-0"
       x-transition:leave-end="opacity-0 translate-y-full"
+      @click.once="$refs.searchInput.focus()"
       @click.stop
       class="bg-white p-6 rounded-t-lg md:rounded-lg shadow-xl max-w-screen-xl w-full h-full overflow-auto"
     >
