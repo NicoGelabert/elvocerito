f873f232417abbb0253a7894b5ea588e3fea04ed
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2026-01-11 20:34:32 +0100

    Fix Tags index. Order

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::orderBy('created_at', 'desc')->get();
         return view('news.index', [
             'articles' => $articles
         ]);
