d8e3b25fddb89a4d27558d77d46bd315c59d1a8e
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2026-06-01 13:27:15 +0200

    Loading cards II

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,5 +38,7 @@
         <div id="agente-app">
             <agente-chat></agente-chat>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+<script>eruda.init();</script>
     </body>
 </html>
